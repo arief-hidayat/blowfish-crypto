1f24602d9e52dc03a10dd8514fe953f4ecee226f
arief-hidayat <mr.arief.hidayat@gmail.com>
2014-04-26 06:12:29 +0800

    add test data

--- src/test/java/com/hida/crypto/BlowfishTest.java
+++ src/test/java/com/hida/crypto/BlowfishTest.java
@@ -16,7 +16,13 @@ public class BlowfishTest {
         return new Object[][] {
                 {"IMMS_VALETTA:20140101:20170101", "arief.is.awesome" , "Sn/9lVcPG/89Lqh7GMGptrbKHtc4CbJ/91//rBjz/Ko=" },
                 {"IMMS_JAKARTA:20140101:20170101", "arief.is.awesome" , "nvl9aVLek50UPFCAHxlNVrbKHtc4CbJ/91//rBjz/Ko=" },
-                {"IMMS:20140101:20200101", "arief.is.awesome" , "ok3rZJBiC7q2yh7XOAmyfwav/sJwxY5v" }
+                {"IMMS:20140101:20200101", "arief.is.awesome" , "ok3rZJBiC7q2yh7XOAmyfwav/sJwxY5v" },
+
+                {"IMMS_VALETTA:20140101:20170101", "portek.it.automation.developer.team" , "kuLy/mu2lnUrPmeGb9Ta7FZXalSEEc5cwnU18kgnuyQ=" },
+                {"IMMS_JAKARTA:20140101:20170101", "portek.it.automation.developer.team" , "OeLk28XeHfzBJiSGWYv8mVZXalSEEc5cwnU18kgnuyQ=" },
+                {"IMMS:20140101:20200101", "portek.it.automation.developer.team" , "SF9AWAjvK8xWV2pUhBHOXOrkUE64Lf1q" },
+
+                {"CTMS:20140101:20200101", "portek.it.automation.developer.team" , "n4Qd0631WpNWV2pUhBHOXOrkUE64Lf1q" }
         };
     }
     @Test(dataProvider = "licenses")
